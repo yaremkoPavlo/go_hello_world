1a61f6d4e71be6d6b16e96a157ee4108c657d6ae
yaremkoPavlo <yaremko.pavlo@gmail.com>
2019-02-08 09:57:39 +0200

    add new func to save file and read file from system

--- cards/deck.go
+++ cards/deck.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -44,4 +46,19 @@ func deal(d deck, handSize int) (deck, deck) {
 
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bt, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - log the error and return a call to newDeck()
+		// Option #2 - log the error and entirely quit the program
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	return strings.Split(string(bt), ",")
 }
--- cards/main.go
+++ cards/main.go
@@ -1,17 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	cards := newDeck()
-
+	//cards := newDeck()
 	// hand, renainingCards := deal(cards, 5)
-
 	// hand.print()
 	// renainingCards.print()
-
-	str := cards.toString()
-	fmt.Println(str)
+	//str := cards.toString()
+	//fmt.Println(str)
+	//cards.saveToFile("my_cards")
+	fmt.Println(newDeckFromFile("my_cards"))
 }
